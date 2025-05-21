14bb8a21438dd231860bcee186cd6e5d6ee8940c
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-05-21 16:10:16 -0400

    feat: Update bastion.sh

--- security-engineering/final_project/bastion.sh
+++ security-engineering/final_project/bastion.sh
@@ -1,6 +1,17 @@
 #!/bin/rbash
 
-PATH=/bin:/usr/bin
+# TODO(perf): 
+#   - Read from an SSH config for list of available connections
+#       - Format?
+#           - Host?
+#           - Hostname?
+#           - User@Hostname?
+#       - Parse with perl, awk, grep
+
+export PATH=/bin:/usr/bin
+
+declare FREE_USER=kolkhis
+declare DESTINATION=192.168.4.11
 
 declare INPUT
 read -r -n 2 -t 20 -p "Welcome!
@@ -10,21 +21,29 @@ Select one of the following:
 2. Exit
 
 > " INPUT
-case $INPUT in
-  1)
-      printf "Going to DestinationHost.\n"
-      ssh freeuser@destinationhost
-      exit 0
-      ;;
-  2)
-      printf "Leaving now.\n"
-      exit 0
-      ;;
-  *)
-      printf "Unknown input. Goodbye.\n"
-      exit 0
-      ;;
-esac
+
+if [[ -n $INPUT ]]; then
+    case $INPUT in
+        1)
+            printf "Going to DestinationHost.\n"
+            ssh "${FREE_USER}@${DESTINATION}"
+            exit 0
+            ;;
+        2)
+            printf "Leaving now.\n"
+            exit 0
+            ;;
+        [^0-9])
+            printf "You can only enter numbers.\n"
+            exit 1
+            ;;
+        *)
+            printf "Unknown input. Goodbye.\n"
+            exit 1
+            ;;
+    esac
+fi
+
 exit 0
 
 
