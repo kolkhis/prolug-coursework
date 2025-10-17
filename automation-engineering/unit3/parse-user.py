3b87bf40bf640fe4f82a0a2c19586759736cca8d
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-10-17 11:55:02 -0400

    feat: Add more indepth user parsing logic

--- automation-engineering/unit3/parse-user.py
+++ automation-engineering/unit3/parse-user.py
@@ -6,6 +6,9 @@ with open('./users.csv', 'r') as fh:
     data = fh.readlines()
 
 for ln in csv.reader(data):
-    print(f"Line num: {ln}")
+    gen = ln[0]
+    age = ln[2]
+    loc = ln[3]
+    print(f"Gender: {gen}, Age: {age}, Location: {loc}")
 
 
